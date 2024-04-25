e7c1bdbf07174ef41569f76dc563a57a4010d8f7
JIAO <jiaoyongshun720@gmail.com>
2024-04-25 16:59:41 +0200

    新增员工代码完善1：重复用户异常处理完善

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -22,6 +25,28 @@ public class GlobalExceptionHandler {
     public Result exceptionHandler(BaseException ex){
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
+    }
+
+    /**
+     * 处理 sql 异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        // java.sql.SQLIntegrityConstraintViolationException: Duplicate entry '小红' for key 'employee.idx_username
+        // 获得异常信息（上面就是异常输出的结果，exception: message 的形式）
+        String message = ex.getMessage();
+
+        // 判断这个异常中是否包含 Duplicate entry 字段
+        if(message.contains("Duplicate entry")){
+            String[] s = message.split(" ");
+            String username = s[2];
+            String msg = username + MessageConstant.ALREADY_EXISTS;
+            return Result.error(msg);
+        }else{
+            return Result.error("MessageConstant.UNKNOWN_ERROR");
+        }
     }
 
 }
