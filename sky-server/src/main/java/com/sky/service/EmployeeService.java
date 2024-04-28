4c82bd49dfcfcf00e2313442b7212dc8b25fba47
JIAO <jiaoyongshun720@gmail.com>
2024-04-28 15:16:39 +0200

    根据ID查询员工接口实现

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -38,4 +38,11 @@ public interface EmployeeService {
      * @param id
      */
     void startorstop(Integer status, Long id);
+
+    /**
+     * 根据id查询员工
+     * @param id
+     * @return
+     */
+    Employee getById(Long id);
 }
