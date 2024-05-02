3614d7652ac51b030bf05331d1a5c233b2ed9790
JIAO <jiaoyongshun720@gmail.com>
2024-05-02 05:17:48 +0200

    36.新增菜品_代码开发_2 已经完成了前后端联调，实现了上传图片的功能

--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -35,14 +35,14 @@ public class CommonController {
         log.info("文件上传：{}", file);
 
         try {
-            //原始文件名
+            //原始文件名，上传的文件的名字
             String originalFilename = file.getOriginalFilename();
-            //截取原始文件名的后缀   dfdfdf.png
+            //截取原始文件名的后缀   dfdfdf.png，从最后一个点截取，获取文件名后缀
             String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
             //构造新文件名称 使用 UUID 防止图片命名时重名
             String objectName = UUID.randomUUID().toString() + extension;
 
-            //文件的请求路径
+            //文件的请求路径,第一个参数是文件对象转换成的数组，第二个参数对应的是传上去的图片在 阿里云上的名字
             String filePath = aliOssUtil.upload(file.getBytes(), objectName);
             return Result.success(filePath);
         } catch (IOException e) {
