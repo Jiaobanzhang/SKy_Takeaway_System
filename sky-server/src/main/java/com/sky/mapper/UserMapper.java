c2a3a306302830eb8b548c33dbc16604f1a79c78
JIAO <jiaoyongshun720@gmail.com>
2024-08-19 06:24:43 +0200

    66-84 使用 HttpClient 完成微信小程序登陆功能

---
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -0,0 +1,25 @@
+package com.sky.mapper;
+
+import com.sky.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * @autor: 我亦无他，唯手熟尔
+ */
+@Mapper
+public interface UserMapper {
+    /**
+     * 根据openid查询用户
+     * @param openid
+     * @return
+     */
+    @Select("select * from user where openid = #{openid}")
+    User getByOpenid(String openid);
+
+    /**
+     * 插入数据
+     * @param user
+     */
+    void insert(User user);
+}
